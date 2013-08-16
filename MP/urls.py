c21059187c2adce1aa8b74bd0fc744736eacc5a1
gbarra <gonzalo.barra@usach.cl>
2013-08-16 18:32:30 -0400

    template

--- MP/urls.py
+++ MP/urls.py
@@ -1,5 +1,6 @@
-from django.conf.urls.defaults import patterns,url
+from django.conf.urls import patterns,url
 
 urlpatterns = patterns('MP.views',
-
+	url(r'^$','index_view',name='vista_principal'),
+	url(r'^registro/$','registro_view',name='vista_registro'),
 )    
--- MP/views.py
+++ MP/views.py
@@ -1,1 +1,19 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.shortcuts import get_object_or_404
+from django.template import RequestContext, loader
+from django.core.mail import *
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render_to_response
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from MP.forms import *
+from django.contrib.auth.models import User
+from MP.models import *
+
+def index_view(request):
+	return render_to_response('MP/index.html',context_instance=RequestContext(request))
+
+def registro_view(request):
+	return render_to_response('MP/registro.html',context_instance=RequestContext(request))
