921bc9c5ca491d41785fde08248071fa588d113a
YerkoPalma <yerko.palma@usach.cl>
2013-10-06 22:26:52 -0400

    registro semiresponsivo

--- MP/static/js/script.js
+++ MP/static/js/script.js
@@ -47,21 +47,96 @@ $(document).ready(function() {
     
     $("#id_usuario").css("margin-left","23%");
     
-    var height = $(".form-help-block.right-block").prev().css("height");
-    height = parseInt(height.substring(0, height.length - 2));
+    function manageSize(){
     
-    $(".special-form-pane + .form-help-block").css("height",(height - 15))
+        /*Se estiman a mano los height de cada help-block*/
+        $("#session-help").css("height",$("#session").css("height"));
+        $("#personal-help").css("height",$("#personal").css("height"));
+        $("#contact-help").css("height",$("#contact").css("height"));
+        
+        $("#aspiraciones-help").css("height",$("#aspiraciones").css("height"));
+        
+        $("#escolar-help").css("height",$("#escolar").css("height"));
+        $("#superiores-help").css("height",$("#superiores").css("height"));
+        $("#habilidades-help").css("height",$("#habilidades").css("height"));
+        
+        $("#experiencias-help").css("height",$("#experiencias").css("height"));
+        
+        /*Se hara para cada uno de los textos de ayuda, por separado*/  
+        //Para el fomr de datos ersonales
+        var topPersonal = parseInt($("#session-help").css("top").substring(0, $("#session-help").css("top").length - 2)) 
+                            + parseInt($("#session").css("height").substring(0, $("#session").css("height").length - 2))
+                            + parseInt($("#session").css("margin-top").substring(0, $("#session").css("margin-top").length - 2))
+                            + parseInt($("#session").css("margin-bottom").substring(0, $("#session").css("margin-bottom").length - 2));
+        
+        $("#personal-help").css("top",topPersonal);
+        $("#personal-help").css("left","5%");
+        
+        //Para el form de datos de contacto
+        var topContacto = parseInt($("#personal-help").css("top").substring(0, $("#personal-help").css("top").length - 2)) 
+                            + parseInt($("#personal").css("height").substring(0, $("#personal").css("height").length - 2))
+                            + parseInt($("#personal").css("margin-top").substring(0, $("#personal").css("margin-top").length - 2))
+                            + parseInt($("#personal").css("margin-bottom").substring(0, $("#personal").css("margin-bottom").length - 2));
+        
+        $("#contact-help").css("top",topContacto);
+        $("#contact-help").css("right","5%");
+        
+        //Para el fomr de datos de enseñanza superior
+        var topPersonal = parseInt($("#escolar-help").css("top").substring(0, $("#escolar-help").css("top").length - 2)) 
+                            + parseInt($("#escolar").css("height").substring(0, $("#escolar").css("height").length - 2))
+                            + parseInt($("#escolar").css("margin-top").substring(0, $("#escolar").css("margin-top").length - 2))
+                            + parseInt($("#escolar").css("margin-bottom").substring(0, $("#escolar").css("margin-bottom").length - 2));
+        
+        $("#superiores-help").css("top",topPersonal);
+        $("#superiores-help").css("left","5%");
+        
+        //Para el form de datos de otras habilidades
+        var topContacto = parseInt($("#superiores-help").css("top").substring(0, $("#superiores-help").css("top").length - 2)) 
+                            + parseInt($("#superiores").css("height").substring(0, $("#superiores").css("height").length - 2))
+                            + parseInt($("#superiores").css("margin-top").substring(0, $("#superiores").css("margin-top").length - 2))
+                            + parseInt($("#superiores").css("margin-bottom").substring(0, $("#superiores").css("margin-bottom").length - 2));
+        
+        $("#habilidades-help").css("top",topContacto);
+        $("#habilidades-help").css("right","5%");
+        
+        /* Se calcula la posicion de cada titulo */
+        if(current==0){
+            $("h1#seccion_personal").css("top", parseInt($("#personal-help").css("top").substring(0, $("#personal-help").css("top").length - 2)) - 55);
+            $("h1#seccion_personal").css("left","initial");
+            $("h1#seccion_personal").css("right", parseInt($("#personal").css("width").substring(0, $("#personal").css("width").length - 2)) 
+                                            - parseInt($("h1#seccion_personal").css("width").substring(0, $("h1#seccion_personal").css("width").length - 2))
+                                            - 20);
+                                            
+            $("h1#seccion_contacto").css("top", parseInt($("#contact-help").css("top").substring(0, $("#contact-help").css("top").length - 2)) - 55);
+            $("h1#seccion_contacto").css("left","60px"); 
+        }
+        if(current==2){
+            
+            $("h2#seccion_superiores").css("top", parseInt($("#superiores-help").css("top").substring(0, $("#superiores-help").css("top").length - 2)) - 55);
+            
+            $("h2#seccion_superiores").css("left","initial");
+            $("h2#seccion_superiores").css("right", parseInt($("#superiores").css("width").substring(0, $("#superiores").css("width").length - 2)) 
+                                            - parseInt($("h2#seccion_superiores").css("width").substring(0, $("h2#seccion_superiores").css("width").length - 2))
+                                            - 20);
+                                            
+            $("h2#seccion_habilidades").css("top", parseInt($("#habilidades-help").css("top").substring(0, $("#habilidades-help").css("top").length - 2)) - 55);
+            $("h2#seccion_habilidades").css("left","60px"); 
+        }   
+    };    
     
-    var height_ = $(".form-help-block.left-block").next().css("height");
-    height_ = parseInt(height_.substring(0, height_.length - 2));
     
-    $(".form-help-block.left-block").css("height",(height_ - 15))
     
     //Control del formulario de registro, para que funcione por paginas
     var minVal=0;
     var maxVal=4;
     var current=0;
-
+    
+    manageSize();
+    
+    $(window).resize(function(){
+        manageSize();        
+    });
+    
     //Control de los mensajes de ayuda debajo del header, para que acompañen a 
     //cada pagina del formulario
     var titulos = ["su Perfil","sus Aspiraciones","sus Estudios", "su Experiencia laboral"];
@@ -79,6 +154,7 @@ $(document).ready(function() {
 
     $("#controls > a#next").click(function(event){        
         event.preventDefault();
+        //manageSize();
         $(".form-pane:nth-of-type("+(current+1)+")").removeClass("active-pane").fadeOut();
         $(".form-pane:nth-of-type("+(current+2)+")").addClass("active-pane").fadeIn();
         current++;
@@ -99,6 +175,7 @@ $(document).ready(function() {
     //Se manejan los botones de acceso a las paginas de registro
     $("#controls > a#prev").click(function(event){
         event.preventDefault();
+        //manageSize();
         $(".form-pane:nth-of-type("+(current+1)+")").removeClass("active-pane").fadeOut();
         $(".form-pane:nth-of-type("+(current)+")").addClass("active-pane").fadeIn();
         current--;
